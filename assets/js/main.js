832741fb6eaa15454c08e9d2e95f90f5fcb088e2
Kellinton <kellintonbrasil@gmail.com>
2024-05-03 17:39:45 -0300

    feat: tela com placar, controle de pontos de vida e cronometro

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,10 @@
 // let larguraNavegador = window.innerWidth // 1802
 // console.log(alturaNavegador, larguraNavegador);
 
-let alturaNavegador = window.innerHeight;
-let larguraNavegador = window.innerWidth;
-
+const areaJogo = document.querySelector('.areaJogo');
+let alturaAreaJogo = areaJogo.clientHeight;
+let larguraAreaJogo = areaJogo.clientWidth;
+let vidas = 1;
 
 /* função nomeada (caso queira reutilizar a função em outras partes do código) para ajustar tamanho da área de jogo (onde serão renderizados os objetos) sempre que o jogador aumentar ou diminuir a tela
 function ajustarTamanhoAreaJogo() {
@@ -22,48 +23,79 @@ ajustarTamanhoAreaJogo()
 
 // evento disparado sempre quando redimensiona a tela, a função anônima pega a largura e altura e armazena dentro das variáveis, sempre que a tela for redimensionado para ajustar tamanho da área de jogo (onde serão renderizados os objetos) sempre que o jogador aumentar ou diminuir a tela
 window.addEventListener('resize', () => {
-    alturaNavegador = window.innerHeight
-    larguraNavegador = window.innerWidth
+    /*
+    innerWidth e innerHeight fornecem a largura e altura da janela do navegador, 
+    // clientWidth e clientHeight fornecem a largura e altura da área de conteúdo de 
+    // um elemento, excluindo as barras de rolagem, bordas e margens.
+    // alturaNavegador = window.innerHeight
+    // larguraNavegador = window.innerWidth
+    */
 
-    console.log(`Altura renderizada: ${alturaNavegador}, Largura renderizada ${larguraNavegador}`);
+    alturaAreaJogo = areaJogo.clientHeight;
+    larguraAreaJogo = areaJogo.clientWidth;
+
+
+
+    // console.log(`Altura renderizada: ${alturaAreaJogo}, Largura renderizada ${larguraAreaJogo}`);
 });
 
 function iniciar(){
-    // remover a fruta anterior (caso exista), evitando que seja acumulada várias frutas na tela
+    // o próprio jogo remove a fruta (caso exista)
     if(document.getElementById('fruta')){
         document.getElementById('fruta').remove();
+
+        // vidas
+        if(vidas > 3) {
+            alert('Acabou!')
+        } else {
+            document.getElementById('vida' + vidas).src = './assets/images/coracao_vazio.png';
+            vidas++;
+        }
     }
     
     // pegando as coordenadas onde as frutas irão aparecer na tela, e calculando a posição aleatória X/Y onde a fruta irá aparecer dentro da largura/altura do navegador. o método floor está arredondando para baixo os valores que estão sendo gerados de forma aleatória pelo método random(), subtraindo 50 para que a imagem não ultrapasse as bordas quando for gerada, pois o tamanho da imagem pode acabar fazendo com que a imagem vaze nas bordas da janela do navegador
-    let posicaoX =  Math.floor(Math.random() * larguraNavegador) - 50;
-    let posicaoY = Math.floor(Math.random() * alturaNavegador) - 50;
+    let posicaoX =  Math.floor(Math.random() * larguraAreaJogo) - larguraAreaJogo * 0.10;
+    let posicaoY = Math.floor(Math.random() * alturaAreaJogo) - alturaAreaJogo * 0.10;
     
     // como está sendo subtraindo -50 da posição atual onde a fruta irá aparecer, fazemos uma condição para que a fruta não fique em uma posição menor que 0. se for menor que zero, ela recebe 0, senão ela recebe ela mesma (permanece o valor que lhe tinha sido atribuído).
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
     
-    console.log(`Posição aleatória X: ${posicaoX}, Posição aleatória Y: ${posicaoY}` );
+    // console.log(`Posição aleatória X: ${posicaoX}, Posição aleatória Y: ${posicaoY}` );
     
     // criando um elemento html (img) que será a fruta que será renderizada em posições aleatórias na janela
     let fruta = document.createElement('img'); // criando o elemento
-    let tamanhoFruta = Math.floor(Math.random() * 5 + 3);
+    let tamanhoFruta = Math.floor(Math.random() * 9 + 4);
     // let tamanhoFrutaLevelFacil = 7;
     // let tamanhoFrutaLevelDificil
     // console.log(`tamanho fruta:  ${tamanhoFruta}`);
 
-
+   
     fruta.src = './assets/images/frutas/fruta_' + (Math.floor(Math.random() * 6) + 1) + '.png'; // escolhendo imagens entre 1 e 6 de forma randomica
     fruta.style.width = tamanhoFruta + 'em'; // tamanho da fruta de forma randomica
     fruta.style.position = 'absolute';
     fruta.style.left = posicaoX + 'px';
     fruta.style.top = posicaoY + 'px';
     fruta.id = 'fruta';
+    fruta.style.opacity = 0; // efeito
+    fruta.style.transition = 'opacity 0.7s ease'; // efeito
+
+    fruta.onclick = function () {
+        this.remove(); // remove a própria fruta (img) do HTML ao ser clicada, usando 'this' para referenciar o elemento DOM ao qual o evento foi atribuído (ou seja, a própria fruta)
+    };
     
-    document.body.appendChild(fruta); // adicionando ao body
+
+    // colocando um efeito de opacidade na imagem
+    setTimeout(() => {
+        fruta.style.opacity = 1;
+    }, 100);
+
+    
+    document.querySelector('.areaJogo').appendChild(fruta); // adicionando ao body
     
 };
 
 // criando e removendo uma fruta a cada ciclo de tempo
 setInterval(() => {
     iniciar();
-}, 1000);
+}, 2000);
---
+++ assets/js/style.js
@@ -0,0 +1,56 @@
+// adicionando uma imagem ao body
+
+const body = document.body;
+const molduraAreaJogoStyle = document.querySelector('.molduraAreaJogo');
+const areaJogoStyle = document.querySelector('.areaJogo');
+const molduraPainelStyle = document.querySelector('.molduraPainel');
+const painelStyle = document.querySelector('.painel');
+const vidasStyle = document.querySelector('.vidas');
+const cronometroStyle = document.querySelector('.cronometro');
+
+
+body.style.backgroundImage = 'url("./assets/images/fundo.png")';
+body.style.backgroundRepeat = 'no-repeat';
+body.style.width = '100%';
+body.style.height = '100vh';
+body.style.display = 'flex';
+body.style.flexDirection = 'column';
+body.style.justifyContent = 'space-around';
+body.style.alignItems = 'center';
+body.style.overflow = 'hidden';
+body.style.backgroundSize = 'cover';
+
+molduraAreaJogoStyle.style.display = 'flex';
+molduraAreaJogoStyle.style.justifyContent = 'center';
+molduraAreaJogoStyle.style.alignItems = 'center';
+molduraAreaJogoStyle.style.backgroundImage = 'url("./assets/images/area-jogavel.png")';
+molduraAreaJogoStyle.style.backgroundRepeat = 'no-repeat';
+molduraAreaJogoStyle.style.width = '650px';
+molduraAreaJogoStyle.style.height = '750px';
+
+areaJogoStyle.style.position = 'relative';
+areaJogoStyle.style.width = '80%';
+areaJogoStyle.style.height = '50vh';
+
+molduraPainelStyle.style.display = 'flex';
+molduraPainelStyle.style.backgroundImage = 'url("./assets/images/placar.png")';
+molduraPainelStyle.style.backgroundRepeat = 'no-repeat';
+molduraPainelStyle.style.justifyContent = 'center';
+molduraPainelStyle.style.alignItems = 'center';
+molduraPainelStyle.style.width = '650px';
+molduraPainelStyle.style.height = '170px';
+
+painelStyle.style.display = 'flex';
+painelStyle.style.justifyContent = 'space-between';
+painelStyle.style.alignItems = 'center';
+painelStyle.style.width = '80%';
+painelStyle.style.height = '40px';
+painelStyle.style.backgroundColor = 'green';
+
+
+vidasStyle.style.backgroundColor = 'blue';
+vidasStyle.style.width = '50%';
+
+cronometroStyle.style.backgroundColor = 'red';
+cronometroStyle.style.marginLeft = '40px';
+cronometroStyle.style.width = '50%';

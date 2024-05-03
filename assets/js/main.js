d8dc81ac7ea364bb1cf6557850c37c65d1b73b07
Kellinton <kellintonbrasil@gmail.com>
2024-05-03 14:02:34 -0300

    feat: criando e removendo uma fruta a cada ciclo de tempo (setInterval), e adicionado a lógica de remover uma fruta quando o id dela existir, e não remover caso ela não exista (será adicionada), evitando assim, que sejam geradas diversas frutas sem que a anterior seja removida antes

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,6 @@ ajustarTamanhoAreaJogo()
 
 */
 
-
-
 // evento disparado sempre quando redimensiona a tela, a função anônima pega a largura e altura e armazena dentro das variáveis, sempre que a tela for redimensionado para ajustar tamanho da área de jogo (onde serão renderizados os objetos) sempre que o jogador aumentar ou diminuir a tela
 window.addEventListener('resize', () => {
     alturaNavegador = window.innerHeight
@@ -30,29 +28,42 @@ window.addEventListener('resize', () => {
     console.log(`Altura renderizada: ${alturaNavegador}, Largura renderizada ${larguraNavegador}`);
 });
 
-// pegando as coordenadas onde as frutas irão aparecer na tela, e calculando a posição aleatória X/Y onde a fruta irá aparecer dentro da largura/altura do navegador. o método floor está arredondando para baixo os valores que estão sendo gerados de forma aleatória pelo método random(), subtraindo 50 para que a imagem não ultrapasse as bordas quando for gerada, pois o tamanho da imagem pode acabar fazendo com que a imagem vaze nas bordas da janela do navegador
-let posicaoX =  Math.floor(Math.random() * larguraNavegador) - 50;
-let posicaoY = Math.floor(Math.random() * alturaNavegador) - 50;
+function iniciar(){
+    // remover a fruta anterior (caso exista), evitando que seja acumulada várias frutas na tela
+    if(document.getElementById('fruta')){
+        document.getElementById('fruta').remove();
+    }
+    
+    // pegando as coordenadas onde as frutas irão aparecer na tela, e calculando a posição aleatória X/Y onde a fruta irá aparecer dentro da largura/altura do navegador. o método floor está arredondando para baixo os valores que estão sendo gerados de forma aleatória pelo método random(), subtraindo 50 para que a imagem não ultrapasse as bordas quando for gerada, pois o tamanho da imagem pode acabar fazendo com que a imagem vaze nas bordas da janela do navegador
+    let posicaoX =  Math.floor(Math.random() * larguraNavegador) - 50;
+    let posicaoY = Math.floor(Math.random() * alturaNavegador) - 50;
+    
+    // como está sendo subtraindo -50 da posição atual onde a fruta irá aparecer, fazemos uma condição para que a fruta não fique em uma posição menor que 0. se for menor que zero, ela recebe 0, senão ela recebe ela mesma (permanece o valor que lhe tinha sido atribuído).
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    
+    console.log(`Posição aleatória X: ${posicaoX}, Posição aleatória Y: ${posicaoY}` );
+    
+    // criando um elemento html (img) que será a fruta que será renderizada em posições aleatórias na janela
+    let fruta = document.createElement('img'); // criando o elemento
+    let tamanhoFruta = Math.floor(Math.random() * 5 + 3);
+    // let tamanhoFrutaLevelFacil = 7;
+    // let tamanhoFrutaLevelDificil
+    // console.log(`tamanho fruta:  ${tamanhoFruta}`);
 
-// como está sendo subtraindo -50 da posição atual onde a fruta irá aparecer, fazemos uma condição para que a fruta não fique em uma posição menor que 0. se for menor que zero, ela recebe 0, senão ela recebe ela mesma (permanece o valor que lhe tinha sido atribuído).
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-console.log(`Posição aleatória X: ${posicaoX}, Posição aleatória Y: ${posicaoY}` );
-
-// criando um elemento html (img) que será a fruta que será renderizada em posições aleatórias na janela
-let fruta = document.createElement('img'); // criando o elemento
-let tamanhoFruta = Math.floor(Math.random() * 7 + 3);
-// let tamanhoFrutaLevelFacil = 7;
-// let tamanhoFrutaLevelDificil
-// console.log(`tamanho fruta:  ${tamanhoFruta}`);
-fruta.src = './assets/images/frutas/fruta_' + (Math.floor(Math.random() * 6) + 1) + '.png'; // escolhendo imagens entre 1 e 6 de forma randomica
-fruta.style.width = tamanhoFruta + 'em'; // tamanho da fruta de forma randomica
-fruta.style.position = 'absolute';
-fruta.style.left = posicaoX + 'px';
-fruta.style.top = posicaoY + 'px';
-
-document.body.appendChild(fruta); // adicionando ao body
+    fruta.src = './assets/images/frutas/fruta_' + (Math.floor(Math.random() * 6) + 1) + '.png'; // escolhendo imagens entre 1 e 6 de forma randomica
+    fruta.style.width = tamanhoFruta + 'em'; // tamanho da fruta de forma randomica
+    fruta.style.position = 'absolute';
+    fruta.style.left = posicaoX + 'px';
+    fruta.style.top = posicaoY + 'px';
+    fruta.id = 'fruta';
+    
+    document.body.appendChild(fruta); // adicionando ao body
+    
+};
 
 // criando e removendo uma fruta a cada ciclo de tempo
-
+setInterval(() => {
+    iniciar();
+}, 1000);

4fa64c5ac1dd128e2f65318955d48bacf1ace549
Kellinton <kellintonbrasil@gmail.com>
2024-05-03 13:34:09 -0300

    feat: adicionado o evento ao redimensionar a tela, uma função pega a largura e a altura da tela, que será a área do game, local onde será renderizado as frutas

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,28 @@
 // let larguraNavegador = window.innerWidth // 1802
 // console.log(alturaNavegador, larguraNavegador);
 
-let alturaNavegador = 0;
-let larguraNavegador = 0;
+let alturaNavegador = window.innerHeight;
+let larguraNavegador = window.innerWidth;
 
 
 /* função nomeada (caso queira reutilizar a função em outras partes do código) para ajustar tamanho da área de jogo (onde serão renderizados os objetos) sempre que o jogador aumentar ou diminuir a tela
-    
-    function ajustarTamanhoAreaJogo() {
-         alturaNavegador = window.innerHeight
-         larguraNavegador = window.innerWidth
+function ajustarTamanhoAreaJogo() {
+     alturaNavegador = window.innerHeight
+     larguraNavegador = window.innerWidth
 
-        console.log(alturaNavegador, larguraNavegador);
+    console.log(alturaNavegador, larguraNavegador);
 }
+
+ajustarTamanhoAreaJogo()
 
 */
 
-// função anônima para ajustar tamanho da área de jogo (onde serão renderizados os objetos) sempre que o jogador aumentar ou diminuir a tela
+
+
+// evento disparado sempre quando redimensiona a tela, a função anônima pega a largura e altura e armazena dentro das variáveis, sempre que a tela for redimensionado para ajustar tamanho da área de jogo (onde serão renderizados os objetos) sempre que o jogador aumentar ou diminuir a tela
 window.addEventListener('resize', () => {
     alturaNavegador = window.innerHeight
     larguraNavegador = window.innerWidth
 
-    console.log(alturaNavegador, larguraNavegador);
+    console.log(`Altura renderizada: ${alturaNavegador}, Largura renderizada ${larguraNavegador}`);
 });
